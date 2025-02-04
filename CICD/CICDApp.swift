c43108afe9669d2701d854aa637dd821b2118b6b
jairrgz27 <127373009+jairrgz@users.noreply.github.com>
2025-02-03 22:07:12 -0500

    Initial Commit

---
+++ CICD/CICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDApp.swift
+//  CICD
+//
+//  Created by Jorge Jair Ramirez Gaston Zuloeta on 3/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
