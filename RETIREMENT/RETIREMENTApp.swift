d6ccd7f06a7eae30cc24113a659e607820e55d1f
jairrgz27 <127373009+jairrgz@users.noreply.github.com>
2025-02-09 16:50:00 -0500

    Initial Commit

---
+++ RETIREMENT/RETIREMENTApp.swift
@@ -0,0 +1,17 @@
+//
+//  RETIREMENTApp.swift
+//  RETIREMENT
+//
+//  Created by Jorge Jair Ramirez Gaston Zuloeta on 9/02/25.
+//
+
+import SwiftUI
+
+@main
+struct RETIREMENTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
